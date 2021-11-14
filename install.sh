0fa103333e711810ef372024020638fc5c4c3f96
valovgv <61600149+valovgv@users.noreply.github.com>
2021-11-14 15:16:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ IPATH=/usr/local/bin
 for FILE in *; do
 if ! [ -f $IPATH/$FILE ]; then 
 if [ $FILE != "install.sh" ]; then
-ln -s $FILE $IPATH/$FILE 
+ln -s $(realpath $FILE) $IPATH/$FILE 
 fi
 fi
 done
