c3b94c2dfb7c1f645a42ef9c1b0943a36b984e5c
valovgv <61600149+valovgv@users.noreply.github.com>
2021-11-14 15:07:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ PATH=/usr/local/bin
 
 for FILE in *; do
 echo $PATH/$FILE
-if ! [ -f $PATH/$FILE ]; then ln -s $FILE $PATH/$FILE; fi
+if ! [ -f $PATH/$FILE ]; then 
+if [ "$FILE" -ne "install.sh" ]; then
+ln -s $FILE $PATH/$FILE 
+fi
+fi
 done
